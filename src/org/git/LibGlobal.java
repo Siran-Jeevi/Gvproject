0141595fd4418351f3327d61a636b690f4e14337
Siran-Jeevi <mailtosiranjeevi@gmail.com>
2021-08-24 11:33:05 +0530

    Get Driver method done

--- src/org/git/LibGlobal.java
+++ src/org/git/LibGlobal.java
@@ -2,6 +2,19 @@ package org.git;
 
 public class LibGlobal {
 	
+	public void getDriver() {
+
+		System.out.println("This is Kamal Branch");
+		
+		
+	}
 	
+	
+	public static void main(String[] args) {
+		
+		LibGlobal libGlobal=new LibGlobal();
+		libGlobal.getDriver();
+		
+	}
 
 }
