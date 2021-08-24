88d3aa6a97ba7606156f2e501d5febb41cae841e
Siran-Jeevi <mailtosiranjeevi@gmail.com>
2021-08-24 11:39:24 +0530

    Load Url Uploaded

--- src/org/git/LibGlobal.java
+++ src/org/git/LibGlobal.java
@@ -2,6 +2,20 @@ package org.git;
 
 public class LibGlobal {
 	
+	private void launchUrl() {
+		System.out.println("this is Rajini Branch");
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		LibGlobal l=new LibGlobal(); 
+		l.launchUrl();
+		
+	}
 	
 
 }
